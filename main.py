92d12b672da51df77c89839ae9370212afaf37cf
Oleh Suvorov <olegsuv.ukr@gmail.com>
2022-10-24 22:24:21 +0300

    modified minimal with *args

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ for i in range(n):
     randomized_list2.append(random.randint(-10, 10))
 
 
-def minimal(list1: list, list2: list) -> int:
+def minimal(*list) -> int:
     minimal_n = 0
-    for i in list1:
-        if i not in list2:
+    print(f"Lists: {list}")
+
+    for i in list[0]:
+        if i not in list[1]:
             if i < minimal_n:
                 minimal_n = i
     return minimal_n
 
 
-print(f"List 1: {randomized_list1}")
-print(f"List 2: {randomized_list2}")
 print(f"Minimal number: {minimal(randomized_list1, randomized_list2)}")
